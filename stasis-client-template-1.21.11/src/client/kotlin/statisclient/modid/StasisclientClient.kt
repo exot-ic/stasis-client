be4484cd6c03ce6eb9ed753cb32769d451788709
exotic makes games <146154054+exot-ic@users.noreply.github.com>
2026-01-28 21:31:08 +1100

    Update StasisclientClient.kt

--- stasis-client-template-1.21.11/src/client/kotlin/statisclient/modid/StasisclientClient.kt
+++ stasis-client-template-1.21.11/src/client/kotlin/statisclient/modid/StasisclientClient.kt
@@ -6,4 +6,11 @@ object StasisclientClient : ClientModInitializer {
 	override fun onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
+	// Top of your Client class
+val mappingKey = KeyBindingHelper.registerKeyBinding(KeyBinding(
+    "key.stasis.clickgui", 
+    InputUtil.Type.KEYSYM, 
+    GLFW.GLFW_KEY_RIGHT_SHIFT, 
+    "category.stasis.client"
+))
 }
