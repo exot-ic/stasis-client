43eba4a616454580ec0ba08b5d27d5afc9a181a2
exotic makes games <146154054+exot-ic@users.noreply.github.com>
2026-01-28 21:01:04 +1100

    Add files via upload
    Fabric mod template (expand on this)

---
+++ stasis-client-template-1.21.11/src/client/kotlin/statisclient/modid/StasisclientClient.kt
@@ -0,0 +1,9 @@
+package statisclient.modid
+
+import net.fabricmc.api.ClientModInitializer
+
+object StasisclientClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
