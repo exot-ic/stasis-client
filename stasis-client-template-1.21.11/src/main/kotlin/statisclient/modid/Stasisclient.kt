1a4c1dad71c23071bcfbfa31d4dd8ef483d51ef9
exotic makes games <146154054+exot-ic@users.noreply.github.com>
2026-01-29 18:39:58 +1100

    Update Stasisclient.kt

--- stasis-client-template-1.21.11/src/main/kotlin/statisclient/modid/Stasisclient.kt
+++ stasis-client-template-1.21.11/src/main/kotlin/statisclient/modid/Stasisclient.kt
@@ -1,17 +1,18 @@
 package statisclient.modid
 
-import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.api.ModInitializer
 import statisclient.modid.features.AutoTotem
 
-class StasisClient : ClientModInitializer {
+class Stasisclient : ModInitializer {
     
     companion object {
-        lateinit var INSTANCE: StasisClient
+        lateinit var INSTANCE: Stasisclient
     }
 
+    // This must match the name of the class in features/autototem.kt
     val autoTotem = AutoTotem()
 
-    override fun onInitializeClient() {
+    override fun onInitialize() {
         INSTANCE = this
     }
 }
